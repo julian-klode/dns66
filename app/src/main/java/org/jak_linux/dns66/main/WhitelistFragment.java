julian-klode/dns66

commit 030545b188a13263da770fed654a4e92d0cb1bab

Whitelist: Refactor checked-state handling
Use a CompoundButton.OnCheckedChangeListener to actually do the
state changes instead of the OnClickListener on the view, that's
more clear.

app/src/main/java/org/jak_linux/dns66/main/WhitelistFragment.java
@@ -149,12 +149,17 @@ public class WhitelistFragment extends Fragment {
             layout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    if (MainActivity.config.whitelist.items.contains(entry.getPackageName())) {
-                        MainActivity.config.whitelist.items.remove(entry.getPackageName());
-                        checkBox.setChecked(false);
-                    } else {
+                    checkBox.setChecked(!checkBox.isChecked());
+                }
+            });
+
+            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                    if (checked) {
                         MainActivity.config.whitelist.items.add(entry.getPackageName());
-                        checkBox.setChecked(true);
+                    } else {
+                        MainActivity.config.whitelist.items.remove(entry.getPackageName());
                     }
                     FileHelper.writeSettings(getActivity(), MainActivity.config);
                 }

commit 9ea6b667da748b502bc88055b225b67676acb054

Whitelist: Only call setChecked after setting checked listener
If we call setChecked() before setting the listener, we could
be setting it on a re-used view that still has the old listener
set. This would cause an item to get the state from another item.

Also change the listener to do nothing if the state is the same,
this avoids unneccessary config rewrites.

This fixes #92, a regression introduced in commit
030545b188a13263da770fed654a4e92d0cb1bab.

app/src/main/java/org/jak_linux/dns66/main/WhitelistFragment.java
@@ -143,7 +143,21 @@ public class WhitelistFragment extends Fragment {
             details.setText(entry.getPackageName());
 
             final Switch checkBox = (Switch) convertView.findViewById(R.id.checkbox);
-            checkBox.setChecked(MainActivity.config.whitelist.items.contains(entry.getPackageName()));
+
+            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                    /* No change, do nothing */
+                    if (checked == MainActivity.config.whitelist.items.contains(entry.getPackageName()))
+                        return;
+                    if (checked) {
+                        MainActivity.config.whitelist.items.add(entry.getPackageName());
+                    } else {
+                        MainActivity.config.whitelist.items.remove(entry.getPackageName());
+                    }
+                    FileHelper.writeSettings(getActivity(), MainActivity.config);
+                }
+            });
 
             View layout = convertView.findViewById(R.id.entry);
             layout.setOnClickListener(new View.OnClickListener() {
@@ -153,17 +167,7 @@ public class WhitelistFragment extends Fragment {
                 }
             });
 
-            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                    if (checked) {
-                        MainActivity.config.whitelist.items.add(entry.getPackageName());
-                    } else {
-                        MainActivity.config.whitelist.items.remove(entry.getPackageName());
-                    }
-                    FileHelper.writeSettings(getActivity(), MainActivity.config);
-                }
-            });
+            checkBox.setChecked(MainActivity.config.whitelist.items.contains(entry.getPackageName()));
 
             return convertView;
         }