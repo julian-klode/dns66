e30f9e9f6ecfa4355c658bd59f809c21d104bf0f
Julian Andres Klode <jak@jak-linux.org>
2019-01-15 22:20:52 +0100

    Update copyright years

--- app/src/main/java/org/jak_linux/dns66/main/ExtraBar.java
+++ app/src/main/java/org/jak_linux/dns66/main/ExtraBar.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2016 Julian Andres Klode <jak@jak-linux.org>
+/* Copyright (C) 2016-2019 Julian Andres Klode <jak@jak-linux.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- app/src/main/java/org/jak_linux/dns66/vpn/BootComplete.java
+++ app/src/main/java/org/jak_linux/dns66/vpn/BootComplete.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2016 Julian Andres Klode <jak@jak-linux.org>
+/* Copyright (C) 2016-2019 Julian Andres Klode <jak@jak-linux.org>
  *
  * Derived from AdBuster:
  * Copyright (C) 2016 Daniel Brodie <dbrodie@gmail.com>
